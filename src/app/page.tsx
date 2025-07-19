5a5439dbdc1cbe7bb5a338b04bb6834cb1a1ecbf
tapwaterz <blackrabbitzxx@gmail.com>
2025-07-19 16:44:46 +0700

    Initial commit: Next.js + TailwindCSS setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hello test, this is incarra
+    </div>
+  );
+}
